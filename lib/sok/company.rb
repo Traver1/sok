3d10a0feffc2705f440967c7a42a1aaab6bdc128
Traver1 <wor1d@outlook.jp>
2017-05-29 19:18:05 +0900

    add unit to company

--- lib/sok/company.rb
+++ lib/sok/company.rb
@@ -11,5 +11,26 @@ module Kabu
         "#{code}-#{market}"
       end
     end
+
+    def unit
+      return @unit if @unit
+      s = soks.order('date').last(20)
+      result = gcd_euclid(s[-1].volume, s[-1].volume)
+      s[-20..-1].each do |sok|
+        result = gcd_euclid(sok.volume, result)
+      end
+      @unit = result
+    end
+
+    def gcd_euclid(u, v)
+      u = u.to_f
+      v = v.to_f
+      while (0 != v) do
+        r = u % v
+        u = v
+        v = r
+      end
+      return u;
+    end
   end
 end
