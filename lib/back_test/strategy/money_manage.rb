6688fe2d62c7a512663e07e098c22b94d8803cc8
Traver1 <wor1d@outlook.jp>
2017-05-30 12:16:51 +0900

    strategy/money_manage.rb

--- lib/back_test/strategy/money_manage.rb
+++ lib/back_test/strategy/money_manage.rb
@@ -192,4 +192,95 @@ module Kabu
       end
     end
   end
+
+  class MoneyManageHukusu
+
+    attr_accessor :length, :n
+
+    def initialize
+      @length = 67
+    end
+
+    def set_env(soks, env)
+      env[:soks] = soks
+    end
+
+    def setup
+    end
+
+    def decide(env)
+      soks = env[:soks]
+      capital = env[:capital]
+
+      volume = volume(capital, soks)
+      action(env, volume)
+    end
+
+    def volume(capital, soks)
+      unit = soks[-1].company.unit
+      stocks = (capital.to_f / unit / soks[-1].open ).to_i
+      stocks * unit
+    end
+
+    def action(env, volume)
+      soks = env[:soks]
+      capital = env[:capital]
+      code = env[:code]
+      date = env[:date]
+      positions = env[:positions]
+      closes = Soks.parse(soks,:close)
+      log = closes[-67..-2].log
+      ave,btm,top,dev = log.bol(65,1)
+
+      actions = []
+      if positions.any?
+        positions.each do |position|
+          gain = position.gain(soks[-1].close,1) / position.price * 100
+          if gain > 20 or gain < -1
+            if  position.sell?
+              actions << Action::Buy.new(code,date,soks[-1].open,position.volume)
+            elsif position.buy?
+              actions << Action::Sell.new(code,date,soks[-1].open,position.volume)
+            end
+          elsif gain > 10
+            if log[-1] < btm[-1] and position.sell?
+              actions << Action::Buy.new(code,date,soks[-1].open,position.volume + volume)
+            elsif log[-1] > top[-1] and position.buy?
+              actions << Action::Sell.new(code,date,soks[-1].open,position.volume + volume)
+            end
+          elsif gain > 2
+            bol = closes[-26..-2].bol(25,2.0)
+            if bol[1][-1] > closes[-2] and position.sell?
+              actions << Action::Buy.new(code,date,soks[-1].open,position.volume)
+            elsif bol[2][-1] < closes[-2] and position.buy?
+              actions << Action::Sell.new(code,date,soks[-1].open,position.volume)
+            end
+          end
+        end
+      end
+
+      is_hold_buy = positions.select {|action| action.buy?}.any?
+      is_hold_sell = positions.select {|action| action.sell?}.any?
+
+      if actions.empty? and (is_hold_buy or is_hold_sell)
+        actions << Action::None.new(code,soks[-1].open)
+      end
+
+      is_buy = actions.select {|action| action.buy?}.any?
+      is_sell = actions.select {|action| action.sell?}.any?
+      if volume > 0
+        if log[-1] < btm[-1] and (positions.empty? or not is_hold_sell) and not is_sell
+          actions << Action::Buy.new(code,date,soks[-1].open,volume)
+        elsif log[-1] > top[-1] and  (positions.empty? or not is_hold_buy) and not is_buy
+          actions << Action::Sell.new(code,date,soks[-1].open,volume)
+        end
+      end
+
+      if actions.empty?
+        actions << Action::None.new(code,soks[-1].open)
+      end
+
+      actions
+    end
+  end
 end
