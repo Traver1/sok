1b8011d1100565ce9f625b13b67552e0733d4795
Traver1 <wor1d@outlook.jp>
2017-06-02 13:57:39 +0900

    add vidya

---
+++ lib/back_test/strategy/vidya.rb
@@ -0,0 +1,107 @@
+module Kabu
+
+  class Vidya
+
+    attr_accessor :length, :l_len, :s_len
+
+    def initialize
+      @length = 40
+      @l_len = 39
+      @s_len = 7
+      @alpha = 2.0 / (1 + 39)
+      @vidya = nil
+    end
+
+    def set_env(soks, env)
+      env[:closes] = Soks.parse(soks[0..-2],:close)
+      env[:open] = soks[-1].open
+    end
+
+    def setup
+      @vidya = nil
+    end
+
+    def decide(env)
+      code = env[:code]
+      open = env[:open]
+      date = env[:date]
+      position = env[:position]
+      closes = env[:closes]
+
+      @vidya = calc_ave(closes)
+      is_buy = closes[-1] > @vidya
+      is_sell = closes[-1] < @vidya
+
+      if position
+        if is_buy and position.sell?
+          return Action::Buy.new(code, date, open, 2)
+        elsif is_sell and position.buy?
+          return Action::Sell.new(code, date, open, 2)
+        else
+          return Action::None.new(code,open)
+        end
+      end
+
+      if is_buy
+        return Action::Buy.new(code, date, open, 1)
+      elsif is_sell
+        return Action::Sell.new(code, date, open, 1)
+      else
+        return Action::None.new(code,open)
+      end
+    end
+
+    def calc_ave(closes)
+      @pvidya = @vidya
+      if @vidya.nil?
+        @vidya = closes[-1]
+      else
+        sdev = closes[-@s_len..-1].dev(@s_len)[-1]
+        ldev = closes[-@l_len..-1].dev(@l_len)[-1]
+        vi = sdev / ldev
+        vi = 1 if vi > 1
+        @vidya = @vidya + @alpha * vi * (closes[-1] - @vidya)
+      end
+      @vidya
+    end
+  end
+
+  class VidyaN < Vidya
+
+    attr_accessor :length, :l_len, :s_len, :n
+
+    def decide(env)
+      code = env[:code]
+      open = env[:open]
+      date = env[:date]
+      position = env[:position]
+      closes = env[:closes]
+
+      @vidya = calc_ave(closes)
+      is_buy = closes[-1] > @vidya
+      is_sell = closes[-1] < @vidya
+
+      if position
+        if position.sell? and position.term >= @n
+          return Action::Buy.new(code, date, open, 1)
+        elsif position.buy? and position.term >= @n
+          return Action::Sell.new(code, date, open, 1)
+        else
+          return Action::None.new(code,open)
+        end
+      end
+
+      return Action::None.new(code,open) if not @pvidya 
+      is_buy_p = closes[-2] > @pvidya
+      is_sell_p = closes[-2] < @pvidya
+
+      if is_buy and not is_buy_p
+        return Action::Buy.new(code, date, open, 1)
+      elsif is_sell and not is_sell_p
+        return Action::Sell.new(code, date, open, 1)
+      else
+        return Action::None.new(code,open)
+      end
+    end
+  end
+end
