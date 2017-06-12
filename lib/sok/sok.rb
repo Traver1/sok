a5a2cb1935b80a5eb72c3ebfa6ba9fc236b0633e
Traver1 <wor1d@outlook.jp>
2017-06-13 04:15:48 +0900

    add adjust

--- lib/sok/sok.rb
+++ lib/sok/sok.rb
@@ -1,5 +1,6 @@
 module Kabu
   class Sok < ActiveRecord::Base
+    attr_accessor :rate
     belongs_to :company
     has_one :split
     validates_uniqueness_of :company_id, scope: :date, allow_nil: false
@@ -11,5 +12,27 @@ module Kabu
     def to_s(separator=',')
       [company.to_s, open, high, low, close, volume].join(separator)
     end
+
+    def adjust_values!(r)
+      if split
+        r = 1 if not r
+        @rate = r * split.after / split.before
+        self.open = self.open * @rate
+        self.high = self.high * @rate
+        self.low = self.low * @rate
+        self.close = self.close * @rate
+        self.volume = self.volume / @rate
+      else
+        @rate = 1 if not r or r == 1
+        return if  @rate == 1
+        @rate = r
+        self.open = self.open * r
+        self.high = self.high * r
+        self.low = self.low * r
+        self.close = self.close * r
+        self.volume = self.volume / r
+      end
+    end
+
   end
 end
