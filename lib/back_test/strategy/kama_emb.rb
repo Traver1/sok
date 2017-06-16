f3c2fd82f014f094ef776c27d4031160fc1df300
Traver1 <wor1d@outlook.jp>
2017-06-16 11:44:21 +0900

    addd filter

--- lib/back_test/strategy/kama_emb.rb
+++ lib/back_test/strategy/kama_emb.rb
@@ -2,7 +2,7 @@ module Kabu
 
   class KamaEmb < KamaLama
 
-    attr_accessor :kmasa_l, :t_stop_l
+    attr_accessor :kmasa_l, :t_stop_l, :profit
 
     def initialize
       @kamas = Soks.new
@@ -11,6 +11,28 @@ module Kabu
       @kamas_l = 10
       @dev_r = 0.3
       @length = [@kamas_l,@t_stop_l].max + 2
+      @q = 1000000
+      @profit = 0
+    end
+
+    def <=>(o)
+      if soks and o.soks and soks.length > @length and o.soks.length > @length
+        if soks[-2].volume * soks[-2].close > @q and o.soks[-2].volume * o.soks[-2].close > @q
+          o.profit <=> profit
+        elsif soks[-2].volume * soks[-2].close > @q
+          -1
+        elsif o.soks[-2].volume * o.soks[-2].close > @q
+          1
+        else
+          0
+        end
+      elsif soks and soks.length > @length
+        -1
+      elsif o.soks and o.soks.length > @length
+        1
+      else
+        0
+      end
     end
 
     def m=(m)
@@ -18,41 +40,14 @@ module Kabu
       @length = [@m,@length].max
     end
 
-    def <=>(other)
-      if soks and other.soks
-        if (soks.last.close * soks.last.volume) > 1000000000 and 
-            (other.soks.last.close * other.soks.last.volume)  > 1000000000
-          if soks.length > 12 and other.soks.length  > 12
-            soks.dev(12)[-1] <=> other.soks.dev(12)[-1]
-          elsif soks.length > 12
-            1
-          elsif other.soks.length > 12
-            -1
-          else
-            0
-          end
-        elsif (soks.last.close * soks.last.volume) > 1000000000
-          1
-        elsif (other.soks.last.close * other.soks.last.volume)  > 1000000000
-          -1
-        else 
-          0
-        end
-      else
-        if soks.nil? and other.soks.nil?
-          0
-        elsif soks.nil?
-          -1
-        else
-          1
-        end
-      end
-    end
-
     def decide(env)
       @kama, @lama = calc_ave(closes)
       @kamas << @kama
       return Action::None.new(code,open) if @kamas.length < @kamas_l
+      if position.nil?
+        return Action::None.new(code,open) if soks[-2] and soks[-2].close < 200
+        return Action::None.new(code,open) if soks[-2] and soks[-2].close * soks[-2].volume < @q
+      end
       dev = closes[-@kamas_l..-1].vol(@kamas_l,@kamas)[-1]
       stc = (@kamas.last - @kamas.min ) / (@kamas.max - @kamas.min)  * 100
       is_buy_p = @is_buy
@@ -63,29 +58,46 @@ module Kabu
       @kamas.shift
 
       if capital
-        volume = capital / company.unit / open
+        volume = capital / company.unit / open * 0.5
         volume = volume.to_i * company.unit
+        return Action::None.new(code,open) if position.nil? and volume == 0
       else
         volume = 1
       end
 
       if position
-        gain = position.gain(closes[-1], position.volume)
+        if capital
+          gain = position.gain(closes[-1], 1).to_f / position.price * 100
+        else
+          gain = position.gain(closes[-1], 1)
+        end
 
         if gain > @m_stop
           high = soks[-@t_stop_l..-1].high(@t_stop_l)[-1]
           low = soks[-@t_stop_l..-1].low(@t_stop_l)[-1]
           if position.sell? and high == soks[-1].high
+            @profit += gain / position.term
             return Action::Buy.new(code, date, open, position.volume)
           elsif  position.buy? and low == soks[-1].low
+            @profit += gain/ position.term
             return Action::Sell.new(code, date, open, position.volume)
           else
             return Action::None.new(code,open)
           end
+        elsif gain < -20 or position.term > 40
+          if position.sell?
+            @profit += gain/ position.term
+            return Action::Buy.new(code, date, open, position.volume)
+          elsif position.buy?
+            @profit += gain/ position.term
+            return Action::Sell.new(code, date, open, position.volume)
+          end
         else
           if @is_buy and not is_buy_p and position.sell?
+            @profit += gain/ position.term
             return Action::Buy.new(code, date, open, position.volume + volume)
           elsif @is_sell and not is_sell_p and position.buy?
+            @profit += gain/ position.term
             return Action::Sell.new(code, date, open, position.volume + volume)
           else
             return Action::None.new(code,open)
