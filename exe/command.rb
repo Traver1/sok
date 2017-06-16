a3f23f6b828ddd3062678ab399f2153a3b2dfb55
Traver1 <wor1d@outlook.jp>
2017-06-17 08:28:17 +0900

    add schedule

--- exe/command.rb
+++ exe/command.rb
@@ -35,6 +35,36 @@ class Command
     end
   end
 
+  def schedule
+    binding.pry
+    #codes  = Kabu::KDb.read_codes
+    codes = %w(2540-T 9984-T)
+    reader =  @yahoo
+
+    codes.each do |code_market|
+      code, market = code_market.split('-')
+      company = Kabu::Company.find_by_code code
+      if company
+        from = company.soks.order(:date).last.date + 1
+        to = Date.today
+      else
+        Kabu::Company.new(code: code, market: market).save
+        from = Date.parse('20000101')
+        to = Date.today
+      end
+      while not reader.read_stocks(code, from, to)
+        sleep 60
+      end
+      stc_cnt  = 0 
+      reader.stocks.each do |stock| 
+        stc_cnt += stock.save ? 1 : 0
+      end
+      spt_cnt  = reader.splits.inject(0) {|s, split| s += split.save ? 1 : 0}
+      puts "insert stocks #{code}: #{stc_cnt}/#{reader.stocks.length}"
+      puts "insert splits #{code}: #{spt_cnt}/#{reader.splits.length}"
+    end
+  end
+
 end
 
 command = Command.new
@@ -45,5 +75,8 @@ when "update"
                              "to:",
                              "code:",
                              "codefrom:"))
+when "schedule"
+  command.schedule
 else
+
 end
